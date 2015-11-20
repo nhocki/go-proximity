9e7fd71c9c864c1f6dbe3f4da478d57d1fce0cb0
Nicolás Hock Isaza <nhocki@gmail.com>
2015-11-20 16:59:38 -0500

    Adding LocationSet#Near to look for locations near a point
    Given a point and a radius, we look for all the search intervals and
    query Redis for results.
    
    The `IntervalFinder` type was added. It allows users to change the way
    they look for intervals to query. If no `IntervalFinder` is added to the
    set, the `DefaultIntervalFinder` is used.

--- location_set.go
+++ location_set.go
@@ -2,10 +2,20 @@ package proximity
 
 import "github.com/mmcloughlin/geohash"
 
+// IntervalFinder provides a way to override the way to look for geohashes
+// intervals. Use `proximity.DefaultIntervalFinder` if you don't want to
+// override it.
+type IntervalFinder func(lat, lng, radius float64) []Int64arr
+
+// DefaultIntervalFinder gets intervals from Redis given a radius. It will remove
+// neighborhoods that are right next to each other to do less queries.
+var DefaultIntervalFinder = intervalsFromRadius
+
 // LocationSet is a set of named points inside a Redis database.
 type LocationSet struct {
-	Name   string
-	client Client
+	Name           string
+	client         Client
+	IntervalFinder IntervalFinder
 }
 
 // Add adds a point <lat, lng> with name `name` to the location set.
@@ -15,6 +25,15 @@ type LocationSet struct {
 func (set *LocationSet) Add(name string, lat, lng float64) error {
 	_, err := set.client.ZAdd(set.Name, encode(lat, lng), name)
 	return err
+}
+
+// Near gets the points within <radius> from the <lat, lng> point.
+func (set *LocationSet) Near(lat, lng, radius float64) ([]string, error) {
+	if set.IntervalFinder == nil || &set.IntervalFinder == nil {
+		set.IntervalFinder = DefaultIntervalFinder
+	}
+	intervals := set.IntervalFinder(lat, lng, radius)
+	return query(set.client, set.Name, intervals)
 }
 
 func encode(lat, lng float64) float64 {
--- location_set_test.go
+++ location_set_test.go
@@ -9,43 +9,73 @@ import (
 
 const key = "go-proximity:test-set"
 
-func TestSuccessfulAdd(t *testing.T) {
-	var (
-		lat          = 123.0
-		lng          = -123.0
-		locationName = "Test Location"
-	)
+var (
+	set          *LocationSet
+	lat          = 123.0
+	lng          = -123.0
+	radius       = 5000.0
+	lowerBound   = -1.0
+	upperBound   = 1.0
+	locationName = "Test Location"
+)
 
+func init() {
+	set = &LocationSet{
+		Name: key,
+		IntervalFinder: func(lat, lng, radius float64) []Int64arr {
+			return []Int64arr{
+				Int64arr{int64(lowerBound), int64(upperBound)},
+			}
+		},
+	}
+}
+
+func TestSuccessfulAdd(t *testing.T) {
 	client := &testClient{}
 	client.On("ZAdd", key, encode(lat, lng), locationName).Return(1, nil)
-
-	set := &LocationSet{
-		Name:   key,
-		client: client,
-	}
+	set.client = client
 
 	err := set.Add(locationName, lat, lng)
+
 	client.AssertExpectations(t)
 	assert.Nil(t, err)
 }
 
 func TestUnsuccessfulAdd(t *testing.T) {
-	var (
-		lat          = 123.0
-		lng          = -123.0
-		locationName = "Test Location"
-	)
+	err := errors.New("Some Redis Error")
 
 	client := &testClient{}
-	err := errors.New("Some Redis Error")
 	client.On("ZAdd", key, encode(lat, lng), locationName).Return(0, err)
-
-	set := &LocationSet{
-		Name:   key,
-		client: client,
-	}
+	set.client = client
 
 	err = set.Add(locationName, lat, lng)
+
 	client.AssertExpectations(t)
 	assert.NotNil(t, err)
+}
+
+func TestSuccessfulNear(t *testing.T) {
+	client := &testClient{}
+	client.On("ZRangeByScore", set.Name, lowerBound, upperBound).Return([]string{"Some Value"}, nil)
+	set.client = client
+
+	results, err := set.Near(lat, lng, radius)
+	client.AssertExpectations(t)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Some Value", results[0])
+}
+
+func TestUnsuccessfulNear(t *testing.T) {
+	err := errors.New("Some Redis Error")
+
+	client := &testClient{}
+	client.On("ZRangeByScore", set.Name, lowerBound, upperBound).Return([]string{}, err)
+	set.client = client
+
+	results, err := set.Near(lat, lng, radius)
+	client.AssertExpectations(t)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, len(results))
 }
